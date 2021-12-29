ff1d2fefb6484b7f1c65e126e489f09c6267ccda
Franklin Volcic <fvolcic@umich.edu>
2021-12-29 14:26:30 -0500

    Updated the mnist display example

--- examples/MNIST/mnist_display.cpp
+++ examples/MNIST/mnist_display.cpp
@@ -1,19 +1,27 @@
 /**
  * This example is how to use the MNIST dataset. 
  * 
- * To compile, use "g++ examples/MNIST/mnist_display.cpp src/MNIST/mnist.cpp -o examples/mnist/mnist_display_example"
+ * To compile, use "g++ examples/MNIST/mnist_display.cpp src/mnist/mnist.cpp -o examples/MNIST/mnist_display_example"
  * To run, use "./examples/MNIST/mnist_display_example"
  * 
  */
 
 #include "../../include/mnist/mnist.h"
-#include <random>
-#include <limits>
 #include <iostream>
 
 int main(){
+
     int index = 100; 
+    
+    // Load the MNIST dataset into memory
+    // It is assumed that the MNIST files are in the data directory 
     MNIST_DATASET * dataset = read_dataset(); 
+
+    // Get and display an image from the testing set
     dataset -> display_image(index, dataset->test_images); 
     std::cout << (int) dataset->test_labels[index] << std::endl;
+
+    // Get and display an image from the training set
+    dataset -> display_image(index, dataset->training_images); 
+    std::cout << (int) dataset->training_labels[index] << std::endl;
 }
